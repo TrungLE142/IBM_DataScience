9a672654d269596b827f34e20209ca79ef21c31a
unknown <leductrung142@gmail.com>
2022-05-18 16:36:08 +0200

    modify something

--- list.py
+++ list.py
@@ -4,6 +4,15 @@ from random import random
 from re import L
 import random
 
+""" List
+    - Introduction
+    - Accessing list
+    - Operations
+    - Working with lists
+    - Functions and Methods
+"""
+
+
 
 # lst = [1,2,3,4]
 # lst1 = [1,'hi',3, 14, [1,2,2], "abcd"]
@@ -13,7 +22,7 @@ import random
 # print((lst1[:5]))
 # print(lst1[:-3])
 # print(lst1[3:])
-# print(lst1[::5]) # lay theo khoang cach 5
+# print(lst1[::5]) # take these elements with distance of 5
 
 # list la type modifiable or mutable
 # my_list = [1,'hi',3, 14, [1,2,2], "abcd"]
@@ -28,27 +37,29 @@ import random
 # list.append([1,3])
 # list.append(list1)
 # print(list)
-# list.extend([1,2,3])
+# list.extend([1,2,3]) 
 
-# cong cac list, phai concatenate list vs list
-# print(list + [1,2,3,'a','b'] )
+# concatenate list vs list
+
+lst = [1,2,3,4]
+print(lst + [1,2,3,'a','b'] )
 
 # #insert (index, value)
-# list.insert(2,'insert')
-# list[2:2] = [1,2,3,4]
-# print(list)
+lst.insert(2,'insert')
+lst[2:2] = [1,2,3,4]
+print(lst)
 
 #remove or del elements of list
-# del list[2]
-# del list[-1]
-# del list[:1]
-# del list1
-# list1.remove(2)
-# print(list1)
+del lst[2]
+del lst[-1]
+del lst[:1]
+# del lst1
+lst.remove(2)
+print(lst)
 
-# my_list4 =  list(input("Enter: ").split(","))
-# my_list5 =  [1,2,3,4,6,7,8,'ab','cd']
-# lst = []
+my_list4 =  list(input("Enter: ").split(","))
+my_list5 =  [1,2,3,4,6,7,8,'ab','cd']
+lst = []
 
 
 # while len(lst) <= 4:
@@ -60,34 +71,33 @@ import random
     
 
 # # print(lst)
-# dict = {'a': 5000, 'b':10000}
-# x= dict.keys()
-# for i in x:
-#     dict[i]
+dict = {'a': 5000, 'b':10000}
+x= dict.keys()
+for i in x:
+    dict[i]
+print(f"Dictionary: {dict}")
     
 # print(x)
 A = 'fgex.;'
 B = 'tyf34gdgf;ektufjhgdgex.;.;rtjynur6'
 
-def sep ():
-    A = input("Enter 1st: ")
-    B = input("Enter 2nd: ")
-    AB = []
-    test = False
-    for i in A:
-        for j in B:
-            if i == j and i not in AB:
-                AB.append(i)
-                print(AB)
-                lenAB = len(AB) + A.count(i)
-                print(lenAB)
-    if len(A) == lenAB:
-        print (True)
-    else: 
-        print (False)
+# def sep ():
+#     A = input("Enter 1st: ")
+#     B = input("Enter 2nd: ")
+#     AB = []
+#     test = False
+#     for i in A:
+#         for j in B:
+#             if i == j and i not in AB:
+#                 AB.append(i)
+#                 print(AB)
+#                 lenAB = len(AB) + A.count(i)
+#                 print(lenAB)
+#     if len(A) == lenAB:
+#         print (True)
+#     else: 
+#         print (False)
         
-
-sep()
 
 
 
